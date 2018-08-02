181ca924f9bcb4fc604a79634a2cec40b0cf046e
Adel AlAhmed <aa.alahmed@tamkeentech.sa>
2018-08-02 13:51:11 +0300

    Puts The message

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,9 +1,14 @@
 class PaymentsController < ApplicationController
   def deduct
-    # read the message 
+    # read the message
     # get the debit account ( From )
     # get the credit account ( To )
     # make the settlement
     # return a message. 
+    puts params["body"]
+  end
+
+  def payment_parmas
+    params.permit(:from, :body)
   end
 end
