1ab1e2e818d0a02bb504213324e9b3d0e3cedb61
Adel AlAhmed <aa.alahmed@tamkeentech.sa>
2018-08-02 13:12:08 +0300

    udpated balance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180801155119) do
+ActiveRecord::Schema.define(version: 20180802100425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180801155119) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.float "balance", default: 0.0
     t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
