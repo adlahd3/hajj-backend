42bbec6d8fb46eacdc0fcfb0c2318c65eb7692ce
Adel AlAhmed <aa.alahmed@tamkeentech.sa>
2018-08-02 15:31:35 +0300

    payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,12 +1,24 @@
 class PaymentsController < ApplicationController
   def deduct
-    # read the message
-    # get the debit account ( From )
-    # get the credit account ( To )
-    # make the settlement
-    # return a message. 
-    @textbody = params[:Body]
-    print @textbody
+    # read & parse the message
+  
+    tokens = params[:Body].split("\n")
+    fromID = tokens[0][tokens[0].index(':')+1..-1].to_i
+    toID   = tokens[1][tokens[1].index(':')+1..-1].to_i
+    amount = tokens[2][tokens[2].index(':')+1..-1].to_f
+    
+    # make the transaction
+    Transaction.create({amount: amount, debit_account_id:fromID, credit_account_id: toID, is_paid: true})
+
+    # settle balance
+    debit_account = Account.find_by(id: fromID)
+    debit_account.balance -= amount
+    debit_account.save
+    
+    credit_account = Account.find_by(id: toID)
+    credit_account.balance += amount
+    credit_account.save
+
   end
 
   def payment_parmas
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
-  belongs_to :debit_account,  :class_name => 'Accounts'
-  belongs_to :credit_account, :class_name => 'Accounts'
+  belongs_to :debit_account,  :class_name => 'Account'
+  belongs_to :credit_account, :class_name => 'Account'
   has_many :accounts
 end
